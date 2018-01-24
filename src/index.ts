53b2c61012829f49213a2e46f32519a042a64dc7
Andrew Crites <acrites@mobiquityinc.com>
2018-01-23 19:01:57 -0500

    Improve hash parsing to simplify nav

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,44 @@ import { spawnSync } from 'child_process';
 async function run() {
   const gilt = screen();
   const display = box({
-    tags: true
+    tags: true,
   });
 
   gilt.key(['escape', 'q', 'C-c'], () => process.exit(0));
 
-  const { stdout } = spawnSync('git', ['log']);
+  // const { stdout } = spawnSync('git', ['log']);
+  const { stdout } = spawnSync('git', ['blame', 'src/index.ts']);
   const initialContent = stdout.toString();
-  let { match, offset, content } = parseHash(initialContent);
-  display.setContent(content);
+  const hashes = parseHashes(initialContent);
+  let selectedHash = 0;
+
+  display.setContent(highlightSelection(initialContent, hashes[0].offset));
 
   gilt.key(['j', 'down'], () => {
-    ({ match, offset, content } = parseHash(initialContent, offset));
+    if (selectedHash < hashes.length) {
+      selectedHash++;
 
-    display.setContent(content);
-    gilt.render();
+      display.setContent(highlightSelection(initialContent, hashes[selectedHash].offset));
+      gilt.render();
+    }
+  });
+
+  gilt.key(['k', 'up'], () => {
+    if (selectedHash > 0) {
+      selectedHash--;
+
+      display.setContent(highlightSelection(initialContent, hashes[selectedHash].offset));
+      gilt.render();
+    }
   });
 
   gilt.key(['enter', 'd'], () => {
-    gilt.spawn('git', ['diff', match], {});
+    gilt.spawn('git', ['diff', hashes[selectedHash].hash], {});
+  });
+
+  gilt.key(['c'], () => {
+    gilt.spawn('git', ['checkout', hashes[selectedHash].hash], {});
+    process.exit();
   });
 
   gilt.append(display);
@@ -34,18 +53,18 @@ if (require.main === module) {
   run();
 }
 
-function parseHash(str, startIdx = 0) {
-  let foundOffset;
-  let foundMatch;
-  const content = str.substr(0, startIdx) + str.substr(startIdx).replace(/\b[0-9a-f]{5,40}\b/, (match, offset) => {
-    foundMatch = match;
-    foundOffset = offset + match.length;
-    return `{white-bg}${match}{/}`;
+function parseHashes(str) {
+  const hashes = [];
+  str.replace(/\b[0-9a-f]{5,40}\b/g, (match, offset) => {
+    hashes.push({ hash: match, offset });
   });
 
-  return {
-    match: foundMatch,
-    offset: foundOffset,
-    content,
-  };
+  return hashes;
+}
+
+function highlightSelection(str, offset = 0) {
+  return (
+    str.substr(0, offset) +
+    str.substr(offset).replace(/\b[0-9a-f]{5,40}\b/, '{white-bg}$&{/}')
+  );
 }
