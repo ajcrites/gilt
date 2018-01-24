e7fe173933e57d1094e1bb8ab94849922f050dd8
Andrew Crites <acrites@mobiquityinc.com>
2018-01-24 01:19:11 -0500

    Improve error and special case handling:
    * Allow for more general git commands
    * Allow diffs to display even if they are less than one page
    * Error handling for some cases

--- src/index.ts
+++ src/index.ts
@@ -14,43 +14,53 @@ async function run() {
 
   gilt.key(['escape', 'q', 'C-c'], () => process.exit(0));
 
-  const { stdout } = spawnSync('git', ['-c', 'color.ui=always', 'log']);
-  // const { stdout } = spawnSync('git', ['blame', 'src/index.ts']);
+  const { stdout, stderr } = spawnSync('git', ['-c', 'color.ui=always', ...process.argv.slice(2)]);
+
   const initialContent = escape(stdout.toString()) + '\n\n';
+
+  const error = stderr.toString();
+
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
   const hashes = parseHashes(initialContent);
   let selectedHash = 0;
 
   display.setContent(highlightSelection(initialContent, hashes[0].offset));
 
-  const guessLines = initialContent.substr(hashes[0].offset, hashes[1].offset).match(/\n/g).length;
+  if (hashes.length > 1) {
+    const guessLines = initialContent.substr(hashes[0].offset, hashes[1].offset).match(/\n/g).length;
 
-  gilt.key(['j', 'down'], () => {
-    if (selectedHash < hashes.length) {
-      selectedHash++;
+    gilt.key(['j', 'down'], () => {
+      if (selectedHash < hashes.length - 1) {
+        selectedHash++;
 
-      display.setContent(highlightSelection(initialContent, hashes[selectedHash].offset));
-      while (selectedHash * guessLines > +display.getScroll() - 5 && +display.getScroll() < +display.getScrollHeight()) {
-        display.scroll(guessLines);
+        display.setContent(highlightSelection(initialContent, hashes[selectedHash].offset));
+        while (selectedHash * guessLines > +display.getScroll() - 5 && +display.getScroll() < +display.getScrollHeight()) {
+          display.scroll(guessLines);
+        }
+        gilt.render();
       }
-      gilt.render();
-    }
-  });
+    });
 
-  gilt.key(['k', 'up'], () => {
-    if (selectedHash > 0) {
-      selectedHash--;
+    gilt.key(['k', 'up'], () => {
+      if (selectedHash > 0) {
+        selectedHash--;
 
-      while (selectedHash * guessLines < +display.getScroll() + 5 && display.getScroll() > 0) {
-        display.scroll(-1 * guessLines);
+        while (selectedHash * guessLines < +display.getScroll() + 5 && display.getScroll() > 0) {
+          display.scroll(-1 * guessLines);
+        }
+        display.setContent(highlightSelection(initialContent, hashes[selectedHash].offset));
+        gilt.render();
       }
-      display.setContent(highlightSelection(initialContent, hashes[selectedHash].offset));
-      gilt.render();
-    }
-  });
+    });
+  }
 
   gilt.key(['enter', 'd'], () => {
     program.clear();
-    gilt.spawn('git', ['diff', '-w', hashes[selectedHash].hash], {});
+    gilt.spawn('git', ['-c', 'core.pager=less -+F', 'show', '-w', hashes[selectedHash].hash], {});
   });
 
   gilt.key(['c'], () => {
