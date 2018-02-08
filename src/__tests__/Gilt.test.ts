c03737cd532d8b9d59df7e621b3f22bbb92c6383
Andrew Crites <acrites@mobiquityinc.com>
2018-02-08 12:57:31 -0500

    refactor: using Mobiquity TypeScript configs

--- src/__tests__/Gilt.test.ts
+++ src/__tests__/Gilt.test.ts
@@ -1,14 +1,14 @@
 import { Gilt } from '../Gilt';
 import { Program } from '../command/Program';
 import { Navigator } from '../command/Navigator';
-import { Block } from '../command/block';
+import { Block } from '../command/Block';
 
-const MockNavigator: Navigator = {
+const mockNavigator: Navigator = {
   content: '',
   navigationBlocks: [] as Block[],
   selectedBlockIdx: 0,
 
-  getSelectedBlock: jest.fn(function() {
+  getSelectedBlock: jest.fn(function () {
     return this.navigationBlocks[this.selectedBlockIdx];
   }),
 
@@ -24,7 +24,7 @@ const MockNavigator: Navigator = {
   clear: jest.fn(),
 };
 
-const MockProgram: Program = {
+const mockProgram: Program = {
   start(command: string[]) {
     return command[0];
   },
@@ -37,7 +37,7 @@ const MockProgram: Program = {
 describe('Gilt', () => {
   it('runs log for log command', () => {
     const gilt = new Gilt();
-    const navigator = MockNavigator;
+    const navigator = mockNavigator;
     const listeners = {};
 
     navigator.key = jest.fn((keys, listener) => {
@@ -46,7 +46,7 @@ describe('Gilt', () => {
       });
     });
 
-    const program = MockProgram;
+    const program = mockProgram;
     gilt.program = program;
     gilt.navigator = navigator;
     gilt.content = '2760ad14a1bd475a3ee25a0b20de8490e0f5e0c6';
